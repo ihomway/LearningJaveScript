f5956d1175c0ca3c7aa4ef5ae13600a9157a6fcd
ihomway <ihomway@live.com>
2016-07-26 17:08:11 +0800

    chapter3

---
+++ GettingStarted/dist/chapter3.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// let currentTempC = 22;
+// let targetTempC, room1 = "conference_room_a", room2 = "lobby";
+
+// const ROOM_TEMP_C = 21.5, MAX_TEMP_C = 30;
+
+// let count = 10;
+// const blue = 0x0000ff;
+// const umask = 0o0022;
+// const roomTemp = 21.5;
+// const c = 3.0e6;
+// const e = -1.6e-19;
+// const inf = Infinity;
+// const ninf = -Infinity;
+// const nan = NaN;
+
+// const small = Number.EPSILON;
+// const bigInt = Number.MAX_SAFE_INTEGER;
+// const max = Number.MAX_VALUE;
+// const minInt = Number.MIN_SAFE_INTEGER;
+// const min = Number.MIN_VALUE;
+// const nInf = Number.NEGATIVE_INFINITY;
+// const nan = Number.NaN;
+// const inf = Number.POSITIVE_INFINITY;
+
+// const dialog = 'Sam looked up, and said "hello, old friend!", as Max walked in.';
+// const imperative = "Don't do that!";
+// const dialog1 = "He looked up and said \"don't do that\" to Max.";
+// const s = "In JavaScript, use \\ as an escape character in strings.";
+
+var currentTemp = 19.5;
+var message = "The current temperature is " + currentTemp + "°c";
+var anotherMessage = "The current temperature is " + currentTemp + "°c";
+console.log(anotherMessage);
---
+++ GettingStarted/es6/chapter3.js
@@ -0,0 +1,59 @@
+// let currentTempC = 22;
+// let targetTempC, room1 = "conference_room_a", room2 = "lobby";
+
+// const ROOM_TEMP_C = 21.5, MAX_TEMP_C = 30;
+
+// let count = 10;
+// const blue = 0x0000ff;
+// const umask = 0o0022;
+// const roomTemp = 21.5;
+// const c = 3.0e6;
+// const e = -1.6e-19;
+// const inf = Infinity;
+// const ninf = -Infinity;
+// const nan = NaN;
+
+// const small = Number.EPSILON;
+// const bigInt = Number.MAX_SAFE_INTEGER;
+// const max = Number.MAX_VALUE;
+// const minInt = Number.MIN_SAFE_INTEGER;
+// const min = Number.MIN_VALUE;
+// const nInf = Number.NEGATIVE_INFINITY;
+// const nan = Number.NaN;
+// const inf = Number.POSITIVE_INFINITY;
+
+// const dialog = 'Sam looked up, and said "hello, old friend!", as Max walked in.';
+// const imperative = "Don't do that!";
+// const dialog1 = "He looked up and said \"don't do that\" to Max.";
+// const s = "In JavaScript, use \\ as an escape character in strings.";
+
+// let currentTemp = 19.5;
+// const message = "The current temperature is " + currentTemp + "\u00b0c";
+// const anotherMessage = `The current temperature is ${currentTemp}\u00b0c`;
+// console.log(anotherMessage);
+
+// const multiline = "line 1 \n\
+// line 2";
+// const multiline2 = `line 1
+// line 2`;
+// const multiline3 = `line 1
+//     line 2
+//     line 3`;
+// const multiline4 = "line1\n" +
+//     "line2\n" +
+//     "line3";
+// const multiline5 = 'Current temperature:\n' +
+//     `\t${currentTemp}\u00b0c\n` +
+//     "Don't worry...the heat is on!";
+// console.log(multiline5);
+
+// const RED = Symbol();
+// console.log(RED);
+
+// const obj = {};
+// obj.color = "yellow";
+// obj["not an identifier"] = 3;
+// console.log(obj["not an identifier"]);
+
+const now = new Date(2016, 6, 25);
+console.log(now);

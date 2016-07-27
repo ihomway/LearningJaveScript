7ac2ad4f023c8d4da0932ecb6cb7b449998af810
ihomway <ihomway@live.com>
2016-07-27 16:35:26 +0800

    crownandanchor

---
+++ GettingStarted/es6/CrownAndAnchor.js
@@ -0,0 +1,74 @@
+// function rand(m, n) {
+//     return m + Math.floor((n - m + 1) * Math.random());
+// }
+
+// function randFace() {
+//     return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
+// }
+
+// let funds = 50;
+// let round = 0;
+
+// while (funds > 1 && funds < 100) {
+//     round++;
+//     console.log(`round ${round}`);
+//     console.log(`\tstarting funds: ${funds}p`);
+
+//     // place bets
+//     let bets = {
+//         corwn: 0,
+//         anchor: 0,
+//         heart: 0,
+//         spade: 0,
+//         club: 0, 
+//         diamond: 0
+//     };
+
+//     let totalBet = rand(1, funds);
+//     if (totalBet === 7) {
+//         totalBet = funds;
+//         bets.heart = totalBet;
+//     } else {
+//         let remaining = totalBet;
+
+//         do {
+//             let bet = rand(1, remaining);
+//             let face = randFace();
+//             bets[face] = bets[face] + bet;
+//             remaining = remaining - bet;
+//         } while(remaining > 0);
+//     }
+
+//     funds = funds - totalBet;
+//     console.log('\tbets: ' + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(',') + ` (total: ${totalBet} pence)`);
+//     // roll dice
+//     const hand = [];
+//     for (var roll = 0; roll < 3; roll++) {
+//         hand.push(randFace());
+//     }
+//     console.log(`\thand: ${hand.join(',')}`);
+
+//     // collect winnings
+//     let winnings = 0;
+//     for (var die = 0; die < hand.length; die++) {
+//         var face = hand[die];
+//         if(bets[face] > 0) {
+//             winnings = winnings + bets[face];
+//         }
+//     }
+
+//     funds = funds + winnings;
+//     console.log(`\twinnings: ${winnings}`);
+// }
+
+// console.log(`\tending funs: ${funds}`);
+
+function test() {
+    if (true) {
+        var i = 10;
+    }
+
+    console.log(i);
+}
+
+test();
